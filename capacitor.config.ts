3ec95c3738a81328e3a8629e8d45d24bf5fa1886
D3vJoy <168494262+J01C3@users.noreply.github.com>
2026-05-27 17:37:03 -0300

    Atualizando tab3

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'loja Fake',
+  appId: '.joy.Loja.Fake',
+  appName: 'Loja Fake',
   webDir: 'www'
 };
 
